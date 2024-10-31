73cfe22a79fe40f5e63049b7b151ec4395b5409f
hosseinnourian <hosseinnourian@protonmail.com>
2024-10-31 15:54:01 +0330

    add user model

--- main.go
+++ main.go
@@ -1,5 +1,67 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	_ "github.com/lib/pq"
+)
+
+var Tmpl *template.Template
+var DB *sql.DB
+var Store = sessions.NewCookieStore([]byte("ums"))
+
+const (
+	Host     string = "localhost"
+	Port     int    = 5432
+	User     string = "postgres"
+	Password string = "nourian1999"
+	Dbname   string = "ums"
+)
+
+func init() {
+	Tmpl, _ = template.ParseGlob("template/*.html")
+	//setup sessions
+	Store.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   3600 * 3,
+		HttpOnly: true,
+	}
+}
+
+func initDB() {
+	var err error
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		Host, Port, User, Password, Dbname)
+	DB, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer DB.Close()
+
+	err = DB.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
+
 func main() {
-	
+	initDB()
+	defer DB.Close()
+
+	gRouter := mux.NewRouter()
+
+	gRouter.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		Tmpl.ExecuteTemplate(w, "home.html", nil)
+	})
+
+	http.ListenAndServe(":8080", gRouter)
+
 }
